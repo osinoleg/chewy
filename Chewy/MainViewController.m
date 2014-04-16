e5fa51616fe88a9a49192829845b50be8df0f853
Mitch Harvey <mharvey@machinezone.com>
2014-04-15 21:26:27 -0700

    Fix the ws typo

--- Chewy/MainViewController.m
+++ Chewy/MainViewController.m
@@ -28,7 +28,7 @@
     [self.view addSubview:_chewyView];
     
     UIButton* adminButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    adminButton.frame = CGRectMake(100, 200ws , 60, 20);
+    adminButton.frame = CGRectMake(100, 200, 60, 20);
     [adminButton setTitle:@"Admin" forState:UIControlStateNormal];
     [adminButton addTarget:self action:@selector(showAdminController) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:adminButton];
